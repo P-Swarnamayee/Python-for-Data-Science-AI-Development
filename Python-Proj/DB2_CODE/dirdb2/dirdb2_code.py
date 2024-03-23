2dc737916299a5a57080d6d9105fff683d1e1847
Swarnamayee Polaki <68282061+P-Swarnamayee@users.noreply.github.com>
2024-03-23 16:39:10 -0400

    Add files via upload

---
+++ Python-Proj/DB2_CODE/dirdb2/dirdb2_code.py
@@ -0,0 +1,51 @@
+import sqlite3
+import pandas as pd
+
+# Connect to the SQLite3 service
+conn = sqlite3.connect('STAFF.db')
+
+# Define table parameters ['DEP_ID','DEP_NAME','MANAGER_ID','LOC_ID']
+table_name = 'DEPARTMENTS'
+attribute_list = ['DEP_ID','DEP_NAME','MANAGER_ID','LOC_ID']
+
+# Read the CSV data
+file_path = 'C:\PortfolioProjects\PortfolioProjects\Python_Project_For_DataEngineering\dirdb2\Departments.csv'
+df = pd.read_csv(file_path, names = attribute_list)
+
+# Load the CSV to the database
+df.to_sql(table_name, conn, if_exists = 'replace', index = False)
+print('Table is ready')
+
+# Query 1: Display all rows of the table
+query_statement = f"SELECT * FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+# Query 2: Display only the Department column for the full table.
+query_statement = f"SELECT DEP_NAME FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+# Query 3: Display the count of the total number of rows.
+query_statement = f"SELECT COUNT(*) FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+'''
+# Append data to the table
+data_append.to_sql(table_name, conn, if_exists = 'append', index = False)
+print('Data appended successfully')
+
+# Query 4: Display the count of the total number of rows.
+query_statement = f"SELECT COUNT(*) FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+
+'''
+
+# Close the connection
+conn.close()
